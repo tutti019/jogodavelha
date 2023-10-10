17a61ca0b967bc8d929aae91291f02aa7d467894
arthur_romanovass <129408571+tutti019@users.noreply.github.com>
2023-10-10 12:08:14 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+var jogador ="x";
+
+function jogar (celula){
+
+    if(celula.innerHTML == ""){
+        celula.innerHTML = jogador;
+
+        if (jogador == "x"){
+            jogador = "o";
+        }
+        else {
+            jogador ="x";
+        }
+    }
+
+}
+
+function reiniciar (){
+    window.location.reload();
+}
